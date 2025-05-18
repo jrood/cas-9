8765d39c371a2a150212ad74e8139fca2a1bfb56
John Rood <mr.john.rood@gmail.com>
2025-05-18 05:48:41 -0500

    jsxs export fix,0.0.15

--- jsx-dev-runtime.ts
+++
@@ -1,5 +0,0 @@
-export { jsx as jsxDEV, Fragment } from './jsx-runtime';
-
-export namespace JSX {
-  export type IntrinsicElements = { [tag: string]: Record<string, any> };
-}
--- jsx-runtime.ts
+++ jsx-runtime.ts
@@ -1,4 +1,5 @@
-export { jsx, Fragment } from './dist.js';
+import { jsx, Fragment } from './dist.js';
+export { jsx, jsx as jsxDEV, jsx as jsxs, jsx as jsxsDEV, Fragment };
 
 export namespace JSX {
   export type IntrinsicElements = { [tag: string]: Record<string, any> };
