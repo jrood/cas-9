63fafc0076056a293cca0424c188f6cbfa899353
John Rood <mr.john.rood@gmail.com>
2025-12-15 22:44:50 -0600

    0.0.30

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -9,6 +9,6 @@ type Component = (props: Props) => any;
 export function jsx(t: Tag, p: Props): any;
 export function Fragment(p: Props): any;
 export function render(content: any, container: HTMLElement): void;
-export function signal<T>(value): [() => T, (newValue: T) => void];
+export function signal<T>(value: T): [() => T, (newValue: T) => void];
 export function effect(fn: () => void): void;
 export function untrack<T>(fn: () => T): T;
